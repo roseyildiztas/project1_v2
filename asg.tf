3798ab546e8ad80b8ea2e499b8e5253ce7ef64ec
dskz <dilna3ka@gmail.com>
2022-02-08 19:42:02 +0000

    added wordpress.sh file on asg

--- asg.tf
+++ asg.tf
@@ -1,53 +1,53 @@
 data "aws_ami" "centos" {
-owners      = ["125523088429"]
-most_recent = true
+  owners      = ["125523088429"]
+  most_recent = true
   filter {
-      name   = "name"
-      values = ["CentOS 7.9.2009 *"]
+    name   = "name"
+    values = ["CentOS 7.9.2009 *"]
   }
   filter {
-      name   = "architecture"
-      values = ["x86_64"]
+    name   = "architecture"
+    values = ["x86_64"]
   }
   filter {
-      name   = "root-device-type"
-      values = ["ebs"]
+    name   = "root-device-type"
+    values = ["ebs"]
   }
 }
 resource "aws_launch_template" "webserver" {
-    name_prefix = "${var.namespace}-template"
-    image_id = data.aws_ami.centos.id
-    instance_type = "t2.micro"
-    user_data = filebase64("/home/ec2-user/Terraform-Project/autoscalling/wordpress.sh")
-    key_name = var.ssh_keypair
-    
-    vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  name_prefix   = "${var.namespace}-template"
+  image_id      = data.aws_ami.centos.id
+  instance_type = "t2.micro"
+  user_data     = filebase64("/home/ec2-user/project1_v2/wordpress.sh")
+  key_name      = var.ssh_keypair
+
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 }
 resource "aws_autoscaling_group" "webserver" {
-  name                      = "${var.namespace}-asg"
-  max_size                  = 99
-  min_size                  = 1
-  vpc_zone_identifier       = module.vpc.public_subnets
-  target_group_arns         =  module.alb.target_group_arns
+  name                = "${var.namespace}-asg"
+  max_size            = 99
+  min_size            = 1
+  vpc_zone_identifier = module.vpc.public_subnets
+  target_group_arns   = module.alb.target_group_arns
   launch_template {
-      id = aws_launch_template.webserver.id
-      version = aws_launch_template.webserver.latest_version
+    id      = aws_launch_template.webserver.id
+    version = aws_launch_template.webserver.latest_version
   }
 }
 module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 5.0"
-  name = "${var.namespace}-alb"
+  source             = "terraform-aws-modules/alb/aws"
+  version            = "~> 5.0"
+  name               = "${var.namespace}-alb"
   load_balancer_type = "application"
   vpc_id             = module.vpc.vpc_id
   subnets            = module.vpc.public_subnets
   security_groups    = [module.lb_sg.security_group.id]
   http_tcp_listeners = [
-      {
-          port= 80,
-          protocol = "HTTP"
-          target_group_index = 0
-      }
+    {
+      port               = 80,
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
   ]
   target_groups = [
     {
@@ -63,11 +63,11 @@ resource "aws_security_group" "allow_tls" {
   description = "Allow TLS inbound traffic"
   vpc_id      = module.vpc.vpc_id
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port        = 0
--- variable.tf
+++ variable.tf
@@ -1,13 +1,18 @@
 variable "namespace" {
-    description = "The project namespace for resource naming"
-    default = "threetier"
+  description = "The project namespace for resource naming"
+  default     = "threetier"
 }
 variable "region" {
   description = "AWS region"
-  default = "us-east-1"
+  default     = "eu-west-1"
 }
 variable "ssh_keypair" {
-    description = "SSH keypair to use for autoscaling"
-    default = null
-    type = string
+  description = "SSH keypair to use for autoscaling"
+  default     = null
+  type        = string
+}
+variable "cluster_engine" {
+  description = "Aurora cluster engine"
+  type        = string
+  default     = "MySQL"
 }
