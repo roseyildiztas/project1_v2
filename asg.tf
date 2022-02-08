c2cbd3aea87fafba9fbd5e7066fbef7a23c9d043
Dilnaz Sherali <64107667+dskz@users.noreply.github.com>
2022-02-07 20:53:20 -0800

    Created asg.tf

---
+++ asg.tf
@@ -0,0 +1,82 @@
+data "aws_ami" "centos" {
+owners      = ["125523088429"]
+most_recent = true
+  filter {
+      name   = "name"
+      values = ["CentOS 7.9.2009 *"]
+  }
+  filter {
+      name   = "architecture"
+      values = ["x86_64"]
+  }
+  filter {
+      name   = "root-device-type"
+      values = ["ebs"]
+  }
+}
+resource "aws_launch_template" "webserver" {
+    name_prefix = "${var.namespace}-template"
+    image_id = data.aws_ami.centos.id
+    instance_type = "t2.micro"
+    user_data = filebase64("/home/ec2-user/Terraform-Project/autoscalling/wordpress.sh")
+    key_name = var.ssh_keypair
+    
+    vpc_security_group_ids = [aws_security_group.allow_tls.id]
+}
+resource "aws_autoscaling_group" "webserver" {
+  name                      = "${var.namespace}-asg"
+  max_size                  = 99
+  min_size                  = 1
+  vpc_zone_identifier       = module.vpc.public_subnets
+  target_group_arns         =  module.alb.target_group_arns
+  launch_template {
+      id = aws_launch_template.webserver.id
+      version = aws_launch_template.webserver.latest_version
+  }
+}
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 5.0"
+  name = "${var.namespace}-alb"
+  load_balancer_type = "application"
+  vpc_id             = module.vpc.vpc_id
+  subnets            = module.vpc.public_subnets
+  security_groups    = [module.lb_sg.security_group.id]
+  http_tcp_listeners = [
+      {
+          port= 80,
+          protocol = "HTTP"
+          target_group_index = 0
+      }
+  ]
+  target_groups = [
+    {
+      name_prefix      = "websvr"
+      backend_protocol = "HTTP"
+      backend_port     = 8080
+      target_type      = "instance"
+    }
+  ]
+}
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "allow_tls"
+  }
+}
